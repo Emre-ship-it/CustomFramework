7a1555604a758474d6314731d0529af48d84ccd2
emre.yasa <emre.yasa@accenture.com>
2025-05-07 11:39:27 -0700

    Add sdk file

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFramework
+//
+//  Created by emre.yasa on 5/7/25.
+//
+
+import Foundation
+
+public struct SDK {
+    static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
