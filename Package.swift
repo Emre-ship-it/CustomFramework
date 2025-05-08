0533571fb91f97953b1fb7d22d0c96c403cb3f3f
emre.yasa <emre.yasa@accenture.com>
2025-05-08 15:39:29 -0700

    Get back

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,10 +19,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "CustomFramework",
-            path: "./CustomFramework.xcframework")
-    ]
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .binaryTarget(
+                name: "CustomFramework",
+                path: "./Sources/CustomFramework.xcframework")
+        ]
 )
